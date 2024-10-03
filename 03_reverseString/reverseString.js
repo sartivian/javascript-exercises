eabe21ae30d99e19d5dd99f041e0116405784b4f
Sartivian <fsartivian@gmail.com>
2024-10-03 13:46:12 +0700

    3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    //define a variable to save the reverse string
+    let normalString = word;
+    //take one char at a time from behind
+    let reverseString = "";
+    for (let i = 1; i <= normalString.length; i++){
+        reverseString += normalString.at(-i);
+    }
+    return reverseString;   
 };
 
 // Do not edit below this line

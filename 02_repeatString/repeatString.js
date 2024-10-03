a6c72f6ff4a53c63a8e5a8d26dae99715b3df6ab
Sartivian <fsartivian@gmail.com>
2024-10-03 13:21:50 +0700

    2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,time) {
+    if (time < 0) { return "ERROR"}
+    let string = "";
+    for (let i = 0; i < time; i++){
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line

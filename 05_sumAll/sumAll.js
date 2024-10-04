2ec74dc0661b4dab8c5973eb3997d779cb29874b
Sartivian <fsartivian@gmail.com>
2024-10-04 10:04:40 +0700

    done 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,40 @@
-const sumAll = function() {
+const sumAll = function(beginning,end) {
+    //check data type
+    let beginningType = typeof beginning;
+    let endType = typeof end;
+    if (beginningType !== 'number' || endType !== 'number'){
+        return "ERROR"
+    }
 
+    //return error if negative value
+    if (beginning < 0 || end < 0){
+        return "ERROR"
+    }
+
+    //check integer
+    let beginCheck = Number.isInteger(beginning);
+    let endCheck = Number.isInteger(end);
+    if (beginCheck === false || endCheck === false){
+        return "ERROR"
+    }
+
+    let result = 0
+    if (beginning < end) {
+        for (let i = beginning; i <= end; i++) {
+            result += i;
+            console.log("i : " + i);
+            console.log("result : " + result);
+        }
+        return result;
+    } else if (beginning > end) {
+        for (let i = end; i <= beginning; i++) {
+            result += i;
+            console.log("i : " + i);
+            console.log("result : " + result);
+        }
+        return result;
+    }
+    
 };
 
 // Do not edit below this line
